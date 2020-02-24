320ba8d9f1dbe1b11e5d40186d44302d2d1b8ce7
Paul-Aguilar <47421449+Paul-Aguilar@users.noreply.github.com>
2020-02-24 13:47:41 -0600

    Add files via upload

--- PIT.c
+++ PIT.c
@@ -1,54 +1,51 @@
-/*
- * PIT.c
- *
- *  Created on: 23 feb. 2020
- *      Author: Mike Ortiz
- */
+
 
 #include "MK64F12.h"
 #include "PIT.h"
-#include "Bits.h"
+#include "bits.h"
+
+static volatile uint32_t dummyRead;
+
+static uint8_t g_pit_intr_flag = 0;
 
 void PIT_delay(PIT_timer_t pit_timer, My_float_pit_t system_clock , My_float_pit_t delay)
 {
-	/*initialize the value of the timer*/
-	PIT->CHANNEL[pit_timer]->LDVAL = system_clock/delay;
-	/*timer is enable*/
+	//initialize the value of the timer//
+	PIT->CHANNEL[pit_timer]->LDVAL =system_clock/delay;
+	//timer is enable//
 	PIT->CHANNEL[pit_timer]->TCTRL |= PIT_TCTRL_TEN_MASK;
 }
 
 
 void PIT_clock_gating(void)
 {
-	/*Initialize the clock of the PIT*/
+	//Initialize the clock of the PIT//
 	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
 }
 
 uint8_t PIT_get_interrupt_flag_status(void)
 {
-	uint8_t flag;
-	PIT->CHANNEL[0]->TFLG |= PIT_TFLG_TIF_MASK;
+	return PIT->CHANNEL[0].TFLG;
 }
 
 void PIT_clear_interrupt_flag(void)
 {
-	/*It clear the flag*/
 	PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;
-	flag = PIT->CHANNEL[0].TCTRL;
-
+	dummyRead = PIT->CHANNEL[0].TCTRL; //read control register for clear PIT flag, this is silicon bug
 }
 
 void PIT_enable(void)
 {
-	/*enables the PIT*/
+	//enables the PIT//
 	PIT->MCR |= PIT_MCR_FRZ_SHIFT;
+	PIT->MCR &= ~(PIT_MCR_FRZ_SHIFT);
 }
 
 void PIT_enable_interrupt(PIT_timer_t pit)
 {
-	/*Clears the flag*/
+	/*It clear the flag*/
 	PIT->CHANNEL[pit].TFLG |= PIT_TFLG_TIF_MASK;
-	/*Enables the interrupt*/
+	/*enables the interruption*/
 	PIT->CHANNEL[pit].TCTRL |= PIT_TCTRL_TIE_MASK;
 }
 
